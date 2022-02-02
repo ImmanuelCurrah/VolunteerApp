7bb8fc41d81dcf199f4bbf6b6cf42f187a2b9a3d
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-02-02 14:36:16 -0500

    made card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,19 +1,20 @@
-// import BusinessPostForm from "../Forms/postForm/businessPost/BusinessPostForm"
-// import { LoggedInLayout } from "../Layout/LoggedInLayout";
-// // post handler?
+
+export default function Card(props) {
 
 
-// export default function Card() {
-
-//   const [businessPost, setBusinessPost] = useState();
-//   const token = localStorage.getItem("token");
-
-
-//   return (
-//     <LoggedInLayout>
-//       <div>
-
-//       </div>
-//     </LoggedInLayout>
-//   )
-// }
+  return (
+    <div className="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">{props.eventTitle}</h5>
+        <p className="card-text">{props.eventDescription}</p>
+      </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">Volunteers Needed: {props.volunteersNeeded}</li>
+      </ul>
+      <div className="card-body">
+        <a href="#" className="card-link">Leave a comment</a>
+      </div>
+    </div>
+  )
+};
