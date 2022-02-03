c245435f444a2aea3b4bd90f916799bd3a9341a3
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-02-03 11:32:29 -0500

    fetch all business posts request if functional and rendering to the page. Still needs styling

--- client/src/screens/AllPostsPage/AllPosts.jsx
+++ client/src/screens/AllPostsPage/AllPosts.jsx
@@ -4,19 +4,36 @@ import { fetchBusinessesHandler } from '../../services/apiConfigBusiness/index.j
 export default function AllPosts() {
   const [businessPosts, setBusinessPosts] = useState([]);
 
-  const token = localStorage.getItem('userToken');
-
   useEffect(() => {
+    // variable to grab token out of local storage to pass in as argument in the fetch post request
+    const token = localStorage.getItem('userToken');
+
     const fetchBizPosts = async () => {
       const res = await fetchBusinessesHandler(`${token}`);
-      console.log(res.data);
+      console.log(res.data.data);
+      setBusinessPosts(res.data.data);
     };
     fetchBizPosts();
   }, []);
 
+  const renderAllPosts = (post, index) => {
+    return (
+      <div className="card text-center shadow" key={index}>
+        <div className="overflow">
+          <div className="card-body text-dark">
+            <h3>{post.businessName}</h3>
+            <h5>{post.email}</h5>
+            <p className="post-para">{post.posts}Post Here</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div>
       <h3>All Posts</h3>
+      <div className="all-posts-div">{businessPosts.map(renderAllPosts)}</div>
     </div>
   );
 }
