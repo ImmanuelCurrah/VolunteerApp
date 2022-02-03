2d19b1a9220d279654cc9015f557ee02092bc633
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-02-03 10:12:10 -0500

    commiting due to merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import HomePage from './screens/homePage/HomePage';
 import { Routes, Route } from 'react-router-dom';
 import CreatePost from './screens/createPostPage/CreatePost';
@@ -9,21 +8,9 @@ import UserLogin from './screens/LoginPages/UserLogin/UserLogin';
 import BusinessLogin from './screens/LoginPages/BusinessLogin/BusinessLogin';
 import AllPosts from './screens/AllPostsPage/AllPosts';
 import PageNotFound from './screens/404Page/PageNotFound';
+import Account from './screens/accountPage/Account';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-=======
-import HomePage from "./screens/homePage/HomePage";
-import { Routes, Route } from "react-router-dom";
-import CreatePost from "./screens/createPostPage/CreatePost";
-import SignUpBusiness from "./screens/signUpPage/SignUpBusiness";
-import SignUpUser from "./screens/signUpPage/SignUpUser";
-import Contact from "./screens/ContactPage/Contact";
-import UserLogin from "./screens/LoginPages/UserLogin/UserLogin";
-import BusinessLogin from "./screens/LoginPages/BusinessLogin/BusinessLogin";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import Account from "./screens/accountPage/Account";
->>>>>>> 66e6934db05fb08d900402340f1eb18ebb528746
 
 function App() {
   return (
