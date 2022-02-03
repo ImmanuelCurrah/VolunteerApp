a00603776d254d112f47c82c982f22a398222c95
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-02-03 16:42:00 -0500

    draft of about us page

---
+++ client/src/screens/aboutUsPage/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function AboutUs() {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <div>
+      <p>Hi, we are Immanuel, Kevin, and Laura. We wanted to create a centralized app for non profits to be able to post their events and find volunteers. In this app businesses can create a post to advertise their events and how many volunteers they need. Also, the VolunteerApp is a place where kind hearted people can look for events and activities where their volunteering is needed.</p>
+      </div>
+    </Layout>
+  );
+};
