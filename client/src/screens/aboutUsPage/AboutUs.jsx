13e10731d1bd23594ff9976d1e2afb4d05ef406d
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-02-04 10:16:18 -0500

    styling on about us page, added classNames, marked something as important in sign up css

--- client/src/screens/aboutUsPage/AboutUs.jsx
+++ client/src/screens/aboutUsPage/AboutUs.jsx
@@ -1,11 +1,13 @@
+import { Layout } from "../../components/Layout/Layout";
+import "./AboutUs.css";
 
 export default function AboutUs() {
   return (
     <Layout>
       <div className='about-us'>
-        <h1>About Us</h1>
+        <h1 className="about-title">About Us</h1>
         <div className='paragraph'>
-          <p>Hi, we are Immanuel, Kevin, and Laura. We wanted to create a centralized app for non profits to be able to post their events and find volunteers. In this app businesses can create a post to advertise their events and how many volunteers they need. Also, the VolunteerApp is a place where kind hearted people can look for events and activities where their volunteering is needed. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum labore perspiciatis earum sit, a tempora corrupti, in cumque ex, accusamus provident! Voluptatibus necessitatibus maiores nulla eligendi vitae vel qui nihil?</p>
+          <p className="p-text">Hi, we are Immanuel, Kevin, and Laura. We wanted to create a centralized app for non profits to be able to post their events and find volunteers. In this app businesses can create a post to advertise their events and how many volunteers they need. Also, the VolunteerApp is a place where kind hearted people can look for events and activities where their volunteering is needed. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum labore perspiciatis earum sit, a tempora corrupti, in cumque ex, accusamus provident! Voluptatibus necessitatibus maiores nulla eligendi vitae vel qui nihil?</p>
         </div>
       </div>
     </Layout>
