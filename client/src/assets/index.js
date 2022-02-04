375b911c2d6412803f918310ed1255132f1e52d0
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-02-04 16:40:51 -0500

    got rid of some commented out code, added astronaut picture to about us page

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -2,3 +2,4 @@ export const Volunteer = require('./images/volunteer.jpg');
 export const Balloons = require('./images/balloons.jpg');
 export const Beach = require('./images/beach.jpg');
 export const Desert = require('./images/desert.jpg');
+export const Astros = require("./images/astroscropped.jpg");
