ef39dd4cc6e91886c566038798b963351b4664f2
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-02-07 15:30:52 -0500

    added min-height of 100vh to homepage

--- client/src/screens/AllPostsPage/AllPosts.jsx
+++ client/src/screens/AllPostsPage/AllPosts.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
   fetchBusinessesHandler,
   deleteBusinessPost,
 } from '../../services/apiConfigBusiness/index.js';
 import DeletePostModal from './DeletePostModal.jsx';
-import { useNavigate } from 'react-router-dom';
 import { Layout } from '../../components/Layout/Layout.jsx';
 import { toast } from 'react-toastify';
 import './AllPosts.css';
