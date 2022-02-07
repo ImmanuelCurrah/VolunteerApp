c7635847bb3a17f341e334847ee8f7ab660cdca3
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-02-07 16:46:02 -0500

    added links to our githubs in the footer

--- client/src/UI/footer/Footer.jsx
+++ client/src/UI/footer/Footer.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { Github } from '../../assets/index.js';
 import './Footer.css';
 
 export default function Footer() {
   return (
     <div className="footer">
-      <h5>Created by Immanuel, Laura, Kevin</h5>
+      <div className="footer-header">
+        <h5>Created by{':'}</h5>
+      </div>
+      <div className="footer-links">
+        <div className="footer-immanuel">
+          <Link to="https://github.com/ImmanuelCurrah">
+            <img src={Github} alt="github" />
+          </Link>
+          <h5>Immanuel</h5>
+        </div>
+        <div className="footer-laura">
+          <Link to="https://github.com/laurabow">
+            <img src={Github} alt="github" />
+          </Link>
+          <h5>Laura</h5>
+        </div>
+        <div className="footer-kevin">
+          <Link to="https://github.com/Kevin-c-miller">
+            <img src={Github} alt="github" />
+          </Link>
+          <h5>Kevin</h5>
+        </div>
+      </div>
     </div>
   );
 }
