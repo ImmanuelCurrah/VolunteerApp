24354f3a76f3a53801f87da2943e2f4079860ec6
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-02-07 10:03:24 -0500

    pulling in changes

--- client/src/screens/AllPostsPage/AllPosts.jsx
+++ client/src/screens/AllPostsPage/AllPosts.jsx
@@ -1,52 +1,28 @@
-import { useState, useEffect } from "react";
-import { fetchBusinessesHandler } from "../../services/apiConfigBusiness/index.js";
-import { Layout } from "../../components/Layout/Layout.jsx";
-import "./AllPosts.css";
+import { useState, useEffect } from 'react';
+import { fetchBusinessesHandler } from '../../services/apiConfigBusiness/index.js';
+import { Layout } from '../../components/Layout/Layout.jsx';
+import './AllPosts.css';
 
 export default function AllPosts() {
   const [businessPosts, setBusinessPosts] = useState([]);
 
   useEffect(() => {
     // variable to grab token out of local storage to pass in as argument in the fetch post request
-    const userToken = localStorage.getItem("userToken");
-    const businessToken = localStorage.getItem("businessToken");
+    const userToken = localStorage.getItem('userToken');
+    const businessToken = localStorage.getItem('businessToken');
 
     const fetchBizPosts = async () => {
       const res = await fetchBusinessesHandler(userToken || businessToken);
+      console.log(res.data.data);
       setBusinessPosts(res.data.data);
     };
     fetchBizPosts();
   }, []);
 
-  if (!businessPosts) {
-    return "Loading...";
-  }
-
-  // const renderAllPosts = (post, index) => {
-  //   return (
-  //     <div className="card text-center shadow all-posts-card" key={index}>
-  //       <div className="overflow">
-  //         <div className="card-body text-dark">
-  //           <h3>{post.businessName}</h3>
-  //           <h5>{post.email}</h5>
-
-  //           {post.posts.map((post, index) => {
-  //             return (
-  //               <div key={index}>
-  //                 <h5>{post.event}</h5>
-  //                 <h6>{post.numberNeeded}</h6>
-  //                 <p>{post.content}</p>
-  //               </div>
-  //             );
-  //           })}
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // };
+  // delete post
 
   if (!businessPosts) {
-    return "Loading...";
+    return 'Loading...';
   }
 
   return (
@@ -56,7 +32,6 @@ export default function AllPosts() {
           <h2>Upcoming Events</h2>
           <h6>that are looking for volunteers!</h6>
         </div>
-        {/* <div className="all-posts-div">{businessPosts.map(renderAllPosts)}</div> */}
         {businessPosts.map((posts) => {
           return posts.posts.map((post) => {
             return (
