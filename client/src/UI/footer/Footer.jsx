9bf1a547ccfe0ddc83a780d7708a72f5636b956c
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-02-08 16:17:20 -0500

    fixed up the footer

--- client/src/UI/footer/Footer.jsx
+++ client/src/UI/footer/Footer.jsx
@@ -1,42 +1,88 @@
-import { Github } from '../../assets/index.js';
+import { Github, Linkedin } from '../../assets/index.js';
 import './Footer.css';
 
 export default function Footer() {
   return (
     <div className="footer">
-      <div className="footer-header">
-        <h5>Created by{':'}</h5>
-      </div>
-      <div className="footer-links">
-        <div className="footer-immanuel">
-          <a
-            href="https://github.com/ImmanuelCurrah"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={Github} alt="github" />
-          </a>
-          <h5>Immanuel</h5>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h5 className="footer-header">Created by:</h5>
+          </div>
+          <div className="footer-links">
+            <div className="footer-immanuel">
+              <div className="header-name">
+                <h5>Immanuel Currah</h5>
+              </div>
+              <div className="social">
+                <a
+                  href="https://github.com/ImmanuelCurrah"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={Github} alt="github" />
+                </a>
+
+                <a
+                  href="https://www.linkedin.com/in/immanuel-currah-647420220/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={Linkedin} alt="linkedin" />
+                </a>
+              </div>
+            </div>
+            <div className="footer-laura">
+              <div className="header-name">
+                <h5>Laura Kappa Bowman</h5>
+              </div>
+              <div className="social">
+                <a
+                  href="https://github.com/laurabow"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={Github} alt="github" />
+                </a>
+
+                <a
+                  href="https://www.linkedin.com/in/laura-kappa-bowman/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={Linkedin} alt="linkedin" />
+                </a>
+              </div>
+            </div>
+            <div className="footer-kevin">
+              <div className="header-name">
+                <h5>Kevin Miller</h5>
+              </div>
+              <div className="social">
+                <a
+                  href="https://github.com/Kevin-c-miller"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={Github} alt="github" />
+                </a>
+
+                <a
+                  href="https://www.linkedin.com/in/kevin-c-miller/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={Linkedin} alt="linkedin" />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="footer-laura">
-          <a
-            href="https://github.com/laurabow"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={Github} alt="github" />
-          </a>
-          <h5>Laura</h5>
-        </div>
-        <div className="footer-kevin">
-          <a
-            href="https://github.com/Kevin-c-miller"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={Github} alt="github" />
-          </a>
-          <h5>Kevin</h5>
+        <div className="row footer-bottom">
+          <p className="col-sm">
+            &copy;{new Date().getFullYear()} VolunteerApp
+            <br />
+          </p>
         </div>
       </div>
     </div>
