9469ee735971e7fae32126208039aeea230d9c94
Immanuel Currah <immanueli.currah@gmail.com>
2022-02-09 08:56:16 -0500

    deploy

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,10 @@
-export const Volunteer = require('./images/volunteer.jpg');
-export const Balloons = require('./images/balloons.jpg');
-export const Beach = require('./images/beach.jpg');
-export const Desert = require('./images/desert.jpg');
-export const Astros = require('./images/astroscropped.jpg');
-export const Tree = require('./images/volunteerapp.png');
-export const HelpWanted = require('./images/volunteers-needed.jpg');
-export const Github = require('./images/github.png');
-export const Linkedin = require('./images/linkedin.png');
+export const Volunteer = require("./images/volunteer.jpg");
+export const Balloons = require("./images/balloons.jpg");
+export const Beach = require("./images/beach.jpg");
+export const Desert = require("./images/desert.jpg");
+export const Astros = require("./images/astroscropped.jpg");
+export const Tree = require("./images/volunteerapp.png");
+export const HelpWanted = require("./images/volunteers-needed.jpg");
+export const Github = require("./images/github.png");
+export const Linkedin = require("./images/linkedin.png");
+//merging
